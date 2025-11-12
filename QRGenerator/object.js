c22fd0c62a0b2fc6929fd56c880aac97a98e7022
unknown <sreshthadutta430@gmail.com>
2025-11-12 14:06:33 +0530

    Add remaining files

---
+++ QRGenerator/object.js
@@ -0,0 +1,18 @@
+const student = {
+    name:'Sreshtha Dutta',            // string
+    age:20,                         // integer
+    salary:2600.00,                 // float
+    dob:"18-02-2005",               // date
+    hobbies:["Dancing", "Playing Chess"],        // array
+    address:{
+        city:"Kolkata",
+        pincode:700091
+    }
+}
+
+
+console.log(student);
+
+
+// name = key
+// Sreshtha Dutta value
